231414f3b395231eaf84aeac65d2da0d5d1c7f41
maxminseok <pms950106@gmail.com>
2024-11-03 11:01:25 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.10 ms, Memory: 16.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.swift
@@ -0,0 +1,20 @@
+import Foundation
+
+func solution(_ n:Int) -> Int
+{
+    var charNumber: [Character] = []
+    var stringNumbers = String(n)
+    var sum = 0
+    
+    for char in stringNumbers {
+        charNumber.append(char)
+    }
+    
+    for i in charNumber {
+        if let num = Int(String(i)) {
+            sum += num
+        }
+    }
+    
+    return sum
+}
