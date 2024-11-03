19ad7eb7fd7aae5f4f1940a90a344f79c27fe77e
maxminseok <pms950106@gmail.com>
2024-11-03 09:58:58 +0900

    [level 1] Title: 짝수와 홀수, Time: 0.01 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.swift
+++ 프로그래머스/1/12937. 짝수와 홀수/짝수와 홀수.swift
@@ -1,8 +1,3 @@
 func solution(_ num:Int) -> String {
-    if num % 2 == 1 || num % 2 == -1 {
-        return "Odd"
-    }
-    else {
-        return "Even"
-    }
+    num % 2 == 0 ? "Even" : "Odd"
 }
