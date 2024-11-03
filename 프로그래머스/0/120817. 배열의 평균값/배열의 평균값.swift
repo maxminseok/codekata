6fd7a68bd6bdb62a35e0a3a8bf5dc6d33a9ae6a2
maxminseok <pms950106@gmail.com>
2024-11-03 09:26:47 +0900

    [level 0] Title: 배열의 평균값, Time: 0.03 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.swift
+++ 프로그래머스/0/120817. 배열의 평균값/배열의 평균값.swift
@@ -3,10 +3,10 @@ import Foundation
 let numbers: [Int] = [1, 2, 3, 4, 5, 6, 7, 8 , 9, 10]
 
 func solution(_ numbers:[Int]) -> Double {
-    var total: Double = 0
+    var total = 0
     for number in numbers {
-        total += Double(number)
+        total += number
     }
     
-    return total / Double(numbers.count)
+    return Double(total) / Double(numbers.count)
 }
