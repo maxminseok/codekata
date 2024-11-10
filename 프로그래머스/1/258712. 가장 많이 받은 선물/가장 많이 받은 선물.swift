507f04a86f8f3b384bfd74eb55fd95762038b93e
maxminseok <pms950106@gmail.com>
2024-11-10 17:17:10 +0900

    [level 1] Title: 가장 많이 받은 선물, Time: 58.03 ms, Memory: 17 MB -BaekjoonHub

--- 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.swift
+++ 프로그래머스/1/258712. 가장 많이 받은 선물/가장 많이 받은 선물.swift
@@ -3,61 +3,49 @@ import Foundation
 func solution(_ friends:[String], _ gifts:[String]) -> Int {
     // 이름을 키로 하는 이중 딕셔너리 생성
     var giftRecord: [String : [String:Int]] = [:]
+    var presentIndex: [String : Int] = [:] // 선물 지수
+    var presentCount: [String : Int] = [:] // 다음달 선물 갯수
+    
     for friend in friends {
-        giftRecord["\(friend)"] = [:]
+        giftRecord[friend] = [:]
+        presentIndex[friend] = 0
+        presentCount[friend] = 0
     }
     
     for record in gifts {
         let splitRecord = record.components(separatedBy: " ")
         let giver = splitRecord[0] // 선물 준 사람
-        let reciver = splitRecord[1] // 선물 받은 사람
+        let receiver = splitRecord[1] // 선물 받은 사람
         
-        giftRecord[giver]?[reciver, default: 0] += 1 // [받은 사람: 받은 횟수] 딕셔너리에 키를 reciver로 하는 딕셔너리의 밸류(receiver의 받은 횟수) +1 하기
-    }
-    
-    //선물 지수 계산
-    var givePresentCount: [String : Int] = [:] // 선물 준 횟수 카운트
-    var receivePresentCount: [String : Int] = [:] // 선물 받은 횟수 카운트
-    var presentIndex: [String : Int] = [:] // 선물 지수
-    
-    for (giver, receivers) in giftRecord { // giver - 선물 준 사람, receivers - [giver에게 선물 받은 사람들 : 받은 횟수]
-        for (receiver, count) in receivers { // receiver : 선물 받은 사람, count: receiver가 받은 선물 횟수
-            givePresentCount["\(giver)", default: 0] += count // giver가 receiver들에게 준 선물 갯수 누적 => [giver : count]
-            receivePresentCount["\(receiver)", default: 0] += count // receiver가 giver에게 받은 선물 갯수 누적시키기
-        }
-    }
-    
-    for friend in friends {
-        presentIndex[friend] = (givePresentCount[friend] ?? 0) - (receivePresentCount[friend] ?? 0)
+        giftRecord[giver]?[receiver, default: 0] += 1 // [받은 사람: 받은 횟수] 딕셔너리에 키를 reciver로 하는 딕셔너리의 밸류(receiver의 받은 횟수) +1 하기
+        presentIndex[giver, default: 0] += 1
+        presentIndex[receiver, default: 0] -= 1
     }
     
     // 다음달 선물 갯수
-    var presentCount: [String : Int] = [:] // 다음달 선물 갯수
-    
     for (index, A) in friends.enumerated() {
-        let remainingFriends = friends[(index + 1)...]
-        for B in remainingFriends {
-            let giverPresent = giftRecord[A]?[B] ?? 0 // A가 B에게 준 선물 갯수
-            let receiverPresent = giftRecord[B]?[A] ?? 0 // B가 A에게 준 선물 갯수
+        //let remainingFriends = friends[(index + 1)...]
+        for B in friends[(index + 1)...] {
+            let giftAtoB = giftRecord[A]?[B] ?? 0 // A가 B에게 준 선물 갯수
+            let giftBtoA = giftRecord[B]?[A] ?? 0 // B가 A에게 준 선물 갯수
             
             // A가 B에게 준 선물 갯수가 B가 A에게 준 선물 갯수보다 많을 때
-            if giverPresent > receiverPresent {
-                presentCount["\(A)", default: 0] += 1 // A의 다음달 선물 갯수 +1
+            if giftAtoB > giftBtoA {
+                //presentCount["\(A)", default: 0] += 1 // A의 다음달 선물 갯수 +1
+                presentCount[A]? += 1
             }
-            
-            // A와 B가 주고받은 선물의 갯수가 같을 때 선물 지수 비교
-            else if giverPresent == receiverPresent {
-                if presentIndex["\(A)", default: 0] > presentIndex["\(B)", default: 0] { // A의 선물지수가 B보다 크다면
-                    presentCount["\(A)", default: 0] += 1 // A의 다음달 선물 갯수 +1
-                }
-                else if presentIndex["\(A)", default: 0] < presentIndex["\(B)", default: 0] { // B의 선물지수가 A보다 크다면
-                    presentCount["\(B)", default: 0] += 1 // B의 다음달 선물 갯수 +1
-                }
-            }
-            
             // B가 A에게 준 선물 갯수가 A가 B에게 준 선물 갯수보다 많을 때
+            else if giftAtoB < giftBtoA {
+                presentCount[B]? += 1 // B의 다음달 선물 갯수 +1
+            }
+            // A와 B가 주고받은 선물의 갯수가 같을 때 선물 지수 비교
             else {
-                presentCount["\(B)", default: 0] += 1 // B의 다음달 선물 갯수 +1
+                if presentIndex[A, default: 0] > presentIndex[B, default: 0] { // A의 선물지수가 B보다 크다면
+                    presentCount[A]? += 1 // A의 다음달 선물 갯수 +1
+                }
+                else if presentIndex[A, default: 0] < presentIndex[B, default: 0] { // B의 선물지수가 A보다 크다면
+                    presentCount[B]? += 1 // B의 다음달 선물 갯수 +1
+                }
             }
         }
     }
