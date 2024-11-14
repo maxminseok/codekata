91c65f3b75e815a8ea5065704cb7ace2af7770e7
maxminseok <pms950106@gmail.com>
2024-11-14 09:31:06 +0900

    [level 1] Title: 콜라츠 추측, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.swift
+++ 프로그래머스/1/12943. 콜라츠 추측/콜라츠 추측.swift
@@ -4,20 +4,15 @@ func solution(_ num:Int) -> Int {
     var result = num
     var count = 0
     
-    if num == 1 {
-        return 0
-    }
-    else {
-        while result != 1 && count <= 500 {
-            if result % 2 == 0 {
-                result /= 2
-                count += 1
-            }
-            else {
-                result = result * 3 + 1
-                count += 1
-            }
+    while result != 1 && count <= 500 {
+        if result % 2 == 0 {
+            result /= 2
+            count += 1
         }
-        return count > 500 ? -1 : count
+        else {
+            result = result * 3 + 1
+            count += 1
+        }
     }
+    return count > 500 ? -1 : count
 }
