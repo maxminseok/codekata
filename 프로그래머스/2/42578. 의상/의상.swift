6d834f68fb07c5a28919517cca04d728e3ac370f
maxminseok <pms950106@gmail.com>
2024-11-18 13:45:10 +0900

    [level 2] Title: 의상, Time: 0.08 ms, Memory: 16.5 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.swift
+++ 프로그래머스/2/42578. 의상/의상.swift
@@ -2,21 +2,15 @@ import Foundation
 
 func solution(_ clothes:[[String]]) -> Int {
     var countCategory: [String : Int] = [:]
-    
-    for cloth in clothes {
-        let category = cloth[1]
-        if countCategory.keys.contains(category) {
-            countCategory[category, default: 0] += 1
-        }
-        else {
-            countCategory[category] = 1
-        }
-    }
-    
-    var result = 1
-    for number in countCategory {
-        result *= (number.value + 1)
-    }
-    return result - 1
-}
 
+    for cloth in clothes {
+        let category = cloth[1]     
+        countCategory[category, default: 0] += 1
+    }
+
+    var total = 1
+    for number in countCategory {
+        total *= (number.value + 1)
+    }
+    return total - 1
+}
