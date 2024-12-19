a55da2b151491511fe055881a88267e083d69f3c
maxminseok <pms950106@gmail.com>
2024-12-19 09:23:20 +0900

    [level 2] Title: 카펫, Time: 0.03 ms, Memory: 16.3 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.swift
+++ 프로그래머스/2/42842. 카펫/카펫.swift
@@ -1,25 +1,17 @@
 import Foundation
 
 func solution(_ brown:Int, _ yellow:Int) -> [Int] {
-        
+    
     let total = brown + yellow
 
-    var arr: [(Int, Int)] = []
     for height in 1...Int(sqrt(Double(total))) {
         if total % height == 0 {
             let width = total / height
-            arr.append((width, height))
+            if (width - 2) * (height - 2) == yellow {
+                return [width, height]
+            }
         }
     }
     
-    var result = [Int]()
-    for (width, height) in arr {
-        if (width - 2) * (height - 2) == yellow {
-            result.append(width)
-            result.append(height)
-        }
-        if result.count == 2 { break }
-    }
-    
-    return result
+    return []
 }
