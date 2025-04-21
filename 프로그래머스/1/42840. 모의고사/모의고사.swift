8d4e17359ff93de33804a223c19a10fb3a5e7a01
maxminseok <pms950106@gmail.com>
2025-04-21 16:34:42 +0900

    [level 1] Title: 모의고사, Time: 30.32 ms, Memory: 16.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.swift
+++ 프로그래머스/1/42840. 모의고사/모의고사.swift
@@ -5,20 +5,17 @@ func solution(_ answers:[Int]) -> [Int] {
     let secondPattern: [Int] = [2, 1, 2, 3, 2, 4, 2, 5]
     let thirdPattern: [Int] = [3, 3, 1, 1, 2, 2, 4, 4, 5, 5]
     
-    let firstAnswer = Array(0..<answers.count).map { firstPattern[$0 % firstPattern.count] }
-    let secondAnswer = Array(0..<answers.count).map { secondPattern[$0 % secondPattern.count] }
-    let thirdAnswer = Array(0..<answers.count).map { thirdPattern[$0 % thirdPattern.count] }
+    let patterns = [firstPattern, secondPattern, thirdPattern]
     
-    var count: [Int] = Array(repeating: 0, count: 3)
-    
-    for (index, answer) in answers.enumerated() {
-        if answer == firstAnswer[index] { count[0] += 1 }
-        if answer == secondAnswer[index] { count[1] += 1 }
-        if answer == thirdAnswer[index] { count[2] += 1 }
+    let scores = patterns.map { pattern in
+        zip(answers, (0..<answers.count).map { pattern[$0 % pattern.count] })
+            .filter { $0 == $1 }
+            .count
     }
-
-    let result = count.enumerated()
-        .filter { $0.element == count.max() }
+    
+    let maxScore = scores.max()
+    let result = scores.enumerated()
+        .filter { $0.element == maxScore }
         .map { $0.offset + 1 }
         .sorted()
             
