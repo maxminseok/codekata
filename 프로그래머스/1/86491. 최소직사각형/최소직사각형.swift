cb40ffc23b4ef5d1354e2904a0cea61d91ad8ea7
maxminseok <pms950106@gmail.com>
2025-04-21 14:43:37 +0900

    [level 1] Title: 최소직사각형, Time: 7.78 ms, Memory: 18.4 MB -BaekjoonHub

--- 프로그래머스/1/86491. 최소직사각형/최소직사각형.swift
+++ 프로그래머스/1/86491. 최소직사각형/최소직사각형.swift
@@ -5,21 +5,12 @@ func solution(_ sizes:[[Int]]) -> Int {
     var maxHeight: Int = 0
     
     for size in sizes {
-        guard let maxSize = size.max() else { return 0 }
-        guard let minSize = size.min() else { return 0 }
+        guard let width = size.max() else { return 0 }
+        guard let height = size.min() else { return 0 }
         
-        if (size[0] > maxWidth && size[1] > maxHeight) || (size[0] > maxHeight && size[1] > maxWidth)  {
-            maxWidth = size[0]
-            maxHeight = size[1]
-        }
-        else if maxSize > maxWidth && maxSize > maxHeight {
-            if maxWidth > maxHeight { maxWidth = maxSize }
-            else { maxHeight = maxSize }
-        }
-        else if maxWidth < minSize || maxHeight < minSize {
-            if maxWidth > maxHeight { maxHeight = minSize }
-            else { maxWidth = minSize }
-        }
+        maxWidth = max(maxWidth, width)
+        maxHeight = max(maxHeight, height)
     }
+    
     return maxWidth * maxHeight
 }
